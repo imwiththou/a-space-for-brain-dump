05932d3833b0ec467f185575c5aac77bd70b06c3
Steve <wang0784@e.ntu.edu.sg>
2023-07-13 21:54:05 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
 
 export default function Home() {
+  const sortedPosts = allPosts.sort((a, b) => a.date > b.date)
+
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {sortedPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
