b148e413153eaee309ad6671d4c49262a7606416
Steve <imwiththou@gmail.com>
2023-07-18 14:41:34 +0800

    updated landing page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 </nav>
               </div>
             </header>
-            <main>{children}</main>
+            <main>
+              {children}
             <br></br>
             <span className="text-xs uppercase tracking-wide text-slate-400">&copy; Steve 3202. All rights reserved.</span>
+            </main>
+            
           </div>
           <Analytics />
         </ThemeProvider>
