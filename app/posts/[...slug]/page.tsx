29fb1ef2748bed8f543936f19da90bf0423859e0
Steve <wang0784@e.ntu.edu.sg>
2024-01-17 16:11:19 +0800

    revert back to DAY, MMM DD YYYY format

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -58,7 +58,7 @@ export default async function PostPage({ params }: PostProps) {
         </p>
       )}
       <span className="text-xs text-grey-500/50 uppercase" >
-        {new Date(post.date).toLocaleDateString('en-GB')}
+        {new Date(post.date).toDateString()}
       </span>
       <hr className="my-4" />
       <Mdx code={post.body.code} />
