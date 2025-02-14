f45feee6dc123d3cf494ad7f0c07f2ab32707861
Steve <wang0784@e.ntu.edu.sg>
2025-02-14 12:08:29 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,10 +42,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <main>
               {children}
             <br></br>
-<--
-            <a href="https://notbyai.fyi"><img src="/written-by-human-not-by-ai-white.svg" alt="Written by Human, Not by AI"></img></a> 
-
--->
             <span className="text-xs uppercase tracking-wide text-slate-400">&copy; Steve 5202. All rights reserved.</span>
             </main>
             
