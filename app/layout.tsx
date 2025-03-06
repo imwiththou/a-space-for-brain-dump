d96448c12e0dd0a4394ec3eb64686a83d7b4acd5
Steve <wang0784@e.ntu.edu.sg>
2025-03-06 15:06:04 +0800

    adjust layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="max-w-3xl mx-auto py-10 px-3">
+          <div className="max-w-3xl mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
                 <ModeToggle />
