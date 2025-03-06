6818010be44ef042fb2eee930170ee21b5eef6c1
Steve <wang0784@e.ntu.edu.sg>
2025-03-07 00:36:40 +0800

    Update FigureWithCaption.tsx

--- components/FigureWithCaption.tsx
+++ components/FigureWithCaption.tsx
@@ -8,9 +8,9 @@ interface FigureWithCaptionProps {
 
 const FigureWithCaption: React.FC<FigureWithCaptionProps> = ({ src, alt, caption }) => {
   return (
-    <div className="md:container md:mx-auto box-border p-8 border-0 bg-transparent dark:bg-transparent">
+    <div className="container mx-auto p-8">
       <figure className="rounded-md bg-transparent dark:bg-transparent">
-        <img className="rounded-md shadow-2xl" src={src} alt={alt} />
+        <img className="rounded-md shadow-2xl w-full" src={src} alt={alt} />
         <figcaption className="pt-1 text-left text-xs w-full bg-transparent dark:bg-transparent">
           {caption}
         </figcaption>
